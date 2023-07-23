92f9897ce4110da36a6fbf637931e6310a3fbe8b
nicolaspetermeyer <nicolas.meyer1@outlook.com>
2023-07-23 20:43:11 +0200

    sync

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI, {
-      collection: "drone_object_detection",
+      // collection: "drone_object_detection",
     });
   } catch (err) {
     console.log(err);
