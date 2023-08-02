d40c8eb10a7981bba02adc6c3b71bf7685da0df5
nicolaspetermeyer <nicolas.meyer1@outlook.com>
2023-08-02 20:27:59 +0200

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3002",
+  "http://127.0.0.1:3001",
   "http://127.0.0.1",
 
   "http://172.23.4.80",
