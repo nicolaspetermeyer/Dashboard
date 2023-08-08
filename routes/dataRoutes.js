11da2b0eb7a8608599a684808226152f807330ab
nicolaspetermeyer <nicolas.meyer1@outlook.com>
2023-08-08 22:24:21 +0200

    update

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const dataController = require("../controllers/dataController");
 
-router
-  .route("/")
-  .get(dataController.getAllData)
-  .post(dataController.createData);
+router.route("/").get(dataController.getAllData);
 
 module.exports = router;
