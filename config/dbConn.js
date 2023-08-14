15d59d6095edb30449e410dba1e291f6de0b36a5
PhilippeW <philippewerner.waelchli@uzh.ch>
2023-08-14 08:31:14 +0000

    upload

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       // collection: "drone_object_detection",
     });
   } catch (err) {
